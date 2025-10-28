f7e857a184a2b85b41e95ae59ffc322479153284
vladdiethecoder <156847429+vladdiethecoder@users.noreply.github.com>
2025-10-28 15:32:52 -0400

    Add files via upload

--- dev/ocr_backends/nanonets_ocr2.py
+++ dev/ocr_backends/nanonets_ocr2.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99ac0d89227b1921218cf1dcaa78d7b965b5c3ed8e7e128444ce6145368952db
-size 2860
+oid sha256:b705d4adc4ae49eb5137079cb6f6d2d7f5750a43e5ee4aebe87f1bc2668eb506
+size 3623
